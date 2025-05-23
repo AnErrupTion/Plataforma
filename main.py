48c6375d2d382971af0d3f3c24f90daac254bcc0
Levrape <gabin.dupuis07@gmail.com>
2025-05-23 10:42:18 +0200

    Ajout de la classe Sprite

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pyxel
 
+class Sprite:
+    def __init__(self, x, y, u, v, w, h, colkey=2):
+        self.x, self.y = x, y
+        self.u, self.v = u, v
+        self.w, self.h = w, h
+        self.colkey = colkey
+    
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, self.u, self.v, self.w, self.h, self.colkey)
 
 class App:
     def __init__(self):
